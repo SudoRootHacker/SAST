d081629e9007d638aba49c8ca596336cf8a5f8de
SudoRootHacker <securehacker25@gmail.com>
2026-04-16 15:42:08 +0530

    Update code_smell.php

--- code_smell.php
+++ code_smell.php
@@ -1,24 +1,25 @@
-
 <?php
 
-function complexFunction($a,$b,$c,$d,$e,$f,$g,$h,$i,$j){
+function badFunction($a,$b,$c,$d,$e){
+
+$unused = "test";
 
 if($a==1){
-echo "1";
-}else{
 if($b==2){
-echo "2";
-}else{
 if($c==3){
-echo "3";
-}else{
-echo "other";
+if($d==4){
+if($e==5){
+
+echo "Deep nesting";
+
+}
+}
 }
 }
 }
 
 }
 
-$unusedVariable = 10;
+badFunction(1,2,3,4,5);
 
 ?>
