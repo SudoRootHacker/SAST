d82813114fbe593ccecbf8afedeae8d50d40bf13
SudoRootHacker <securehacker25@gmail.com>
2026-04-16 15:41:53 +0530

    Update log.php

--- log.php
+++ log.php
@@ -1,6 +1,11 @@
-
 <?php
+
 $user = $_GET['user'];
-file_put_contents("app.log", $user . "\n", FILE_APPEND);
+
+$log = "logs.txt";
+
+file_put_contents($log, "User login: " . $user . "\n", FILE_APPEND);
+
 echo "Logged";
+
 ?>
